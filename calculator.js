195fbba9964a2b0817eabfe6a57406f71bfb3820
Ludelaplu <lucien.koot@gmail.com>
2019-05-07 11:24:35 +0200

    calculator werkt, maar alleen in consol html scherm

---
+++ calculator.js
@@ -0,0 +1,57 @@
+function myFunction1() {
+    document.getElementById("demo").value += "1";
+    console.log('1');
+ } 
+ function myFunction2() {
+    document.getElementById("demo").value += "2";
+    console.log('2');
+ } 
+ function myFunctionPlus() {
+    document.getElementById("demo").value += "+";
+    console.log('+');
+ } 
+ function myFunctionTotal() {
+    var sum = document.getElementById("demo").value;
+    console.log(eval(sum));
+ } 
+
+
+
+
+
+ 
+ /*function myFunction1() {
+    document.getElementById("demo").innerHTML = "3";
+    console.log('3');
+ } 
+ function myFunction2() {
+    document.getElementById("demo").innerHTML = "4";
+    console.log('4');
+ } 
+ function myFunction1() {
+    document.getElementById("demo").innerHTML = "5";
+    console.log('5');
+ } 
+ function myFunction2() {
+    document.getElementById("demo").innerHTML = "6";
+    console.log('6');
+ } 
+ function myFunction1() {
+    document.getElementById("demo").innerHTML = "7";
+    console.log('7');
+ } 
+ function myFunction2() {
+    document.getElementById("demo").innerHTML = "4";
+    console.log('8');
+ } 
+ function myFunction1() {
+    document.getElementById("demo").innerHTML = "9";
+    console.log('9');
+ } 
+ function myFunction2() {
+    document.getElementById("demo").innerHTML = "0";
+    console.log('0');
+ }
+ */
+
+
