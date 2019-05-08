28767c3a5460811b5bdd4e905596135b4848a444
Ludelaplu <lucien.koot@gmail.com>
2019-05-08 15:13:05 +0200

    oefenen met loops

--- javascript.js
+++ javascript.js
@@ -5,6 +5,71 @@ let mood = "light";
 console.log(mood);
 
 mood = "dark";
-console.log (mood);
+console.log(mood);
+
+let jansDebt = 150;
+jansDebt = jansDebt - 40;
+console.log(jansDebt);
+
+let lucienTegoed = 300;
+lucienTegoed = lucienTegoed - 243;
+console.log(lucienTegoed);
+
+let lucienKrijgtvanMike = 400;
+lucienKrijgtvanMike = lucienKrijgtvanMike - 100;
+console.log(lucienKrijgtvanMike);
+
+var name = "HARRIET";
+const greeting = "HALLO ";
+console.log(greeting + name);
+
+let groet = "Hallo ", naam = "Joske";
+console.log(groet + naam);
+
+//onderstaand werken allemaal, maar even als comment gezet, anders gaan ze elkaar storen, maar ik wil de functies even bewaren.
+
+
+/*prompt("Enter PINcode \n to get your acces.");
+
+console.log(Math.max(66, 88));
+
+console.log(Math.min(44, 66) / 2);
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " +
+theNumber * theNumber);
+
+let the Number = Number(prompt("wilt u briefjes van: 10\n20\n50?"));
+console.log("Hier is uw te vele geld:" + theNumber * TheNumber);
+
+let num = Number(prompt("Pick a number"));
+if (num < 10) {
+console.log("Small");
+} else if (num < 100) {
+console.log("Medium");
+} else {
+console.log("Large");
+}
+
+*/
+
+
+let number = 0;
+while (number <=33) {
+    console.log(number);
+    number = number + 3;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
